30cef007067d604c5228041242826d5a7de04c7a
Prajwal Tuladhar <praj@infynyxx.com>
2011-08-13 16:45:24 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+version = '1.0.0'
+
+setup(name='sailthru-python-client', 
+        version=version,
+        description='Python client for Sailthru API',
+        long_description=open('./README.md').read(),
+        classifiers=[
+            "License :: OSI Approved :: MIT License",
+            "Topic :: Utilities",
+            "Programming Language :: Python",
+            "Operating System :: OS Independent",
+            "Natural Language :: English",
+            ],
+        keywords='sailthru api',
+        author='Prajwal Tuladhar',
+        author_email='praj@sailthru.com',
+        url='https://github.com/sailthru/sailthru-python-client',
+        license='MIT License',
+        packages=['sailthru'],
+        include_package_data=True,
+        zip_safe=True)
