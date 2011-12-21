767fa4b5c19c50b978bdb1bd6f1ab4cdce8f3412
Prajwal Tuladhar <praj@infynyxx.com>
2011-12-21 14:02:40 -0500

    BUMP 1.0.7

--- sailthru/__init__.py
+++ sailthru/__init__.py
@@ -1,5 +1,5 @@
 from sailthru_client import *
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 __author__ = 'Prajwal Tuladhar'
 __doc__ = """
 This is a Python Sailthru API client based on http://docs.sailthru.com/api
