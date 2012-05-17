dbdc5b762c3a3a5128ee8af7169e54ebfc0aa5c8
Prajwal Tuladhar <praj@infynyxx.com>
2012-05-17 17:46:44 -0400

    SailthruResponse It can now handle response and error. Available methods:     is_ok(): check if the response if ok. Basically it checks for if              respose content has valid JSON and that json doesn't              contain *error* key
        get_body(): Returns JSON dictionary and if as_dictionary is True
                    then, returns raw content
    
        get_response(): Returns response object
                        http://docs.python-requests.org/en/latest/api/#requests.Response
    
        get_status_code(): Returns HTTP status code
    
        get_error(): Returns SailthruReponseError object
    
    SailthruResponseError
    Handles error returned from Sailthru API
        get_message(): Returns error message
    
        get_error_code(): returns error code, if reponse is invalid JSON,
        returns 0

--- sailthru/sailthru_response.py
+++ sailthru/sailthru_response.py
@@ -8,16 +8,21 @@ except ImportError:
 class SailthruResponse(object):
     def __init__(self, response):
         self.response = response
+        self.json_error = False
+        try:
+            self.json = json.loads(response.content)
+        except json.decoder, e:
+            self.json = None
+            self.json_error = str(e)
 
     def is_ok(self):
-        return (self.response.status_code == 200)
+        return self.json_error is not None and not "error" in self.json.keys()
 
     def get_body(self, as_dictionary = True):
-        content = self.response.content
-        if as_dictionary == True:
-            return json.loads(content)
+        if as_dictionary is True:
+            return self.json
         else:
-            return content
+            return self.response.content
 
     def get_response(self):
         return self.response
@@ -25,19 +30,26 @@ class SailthruResponse(object):
     def get_status_code(self):
         return self.response.status_code
 
+    def get_error(self):
+        if self.is_ok():
+            return False
+
+        if self.json_error is not False:
+            code = 0
+            msg = self.json_error
+        else:
+            code = self.json["error"]
+            msg = self.json["errormsg"]
+
+        return SailthruResponseError(msg, code)
+
 class SailthruResponseError(object):
-    def __init__(self, sailthru_response):
-        self.response_dict = sailthru_response.get_body()
+    def __init__(self, message, code):
+        self.message = message
+        self.code = code
 
     def get_message(self):
-        try:
-            return self.response_dict['errormsg']
-        except KeyError:
-            return None
+       return self.message
 
-    def get_code(self):
-        try:
-            return self.response_dict['error']
-        except KeyError:
-            return None
-
+    def get_error_code(self):
+        return self.code
