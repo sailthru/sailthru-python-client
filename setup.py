e9db6170a72e9bcc711fc2153792eda94d174648
Prajwal Tuladhar <praj@infynyxx.com>
2012-06-03 04:04:52 -0400

    make sure setup.py doesn't import sailthru package
    This is required so that, when installing via pip or easy_install, it
    won't throw error: no module name requests
    
    Thanks to @Dannyzen for spotting this issue

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
-import sailthru
 
 setup(name='sailthru-client', 
-        version=sailthru.__version__,
-        packages=find_packages(),
-        description='Python client for Sailthru API',
-        long_description=open('./README.md').read(),
-        classifiers=[
-            "License :: OSI Approved :: MIT License",
-            "Topic :: Utilities",
-            "Programming Language :: Python",
-            "Operating System :: OS Independent",
-            "Natural Language :: English",
-            ],
-        install_requires=[
-            'requests==0.13.0'
-        ],
-        keywords='sailthru api',
-        author='Prajwal Tuladhar',
-        author_email='praj@sailthru.com',
-        url='https://github.com/sailthru/sailthru-python-client',
-        license='MIT License',
-        include_package_data=True,
-        zip_safe=True)
+    version='2.0.1',
+    packages=find_packages(),
+    description='Python client for Sailthru API',
+    long_description=open('./README.md').read(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    install_requires=[
+        'requests >= 0.12.0',
+        'simplejson >= 2.0'
+    ],
+    keywords='sailthru api',
+    author='Prajwal Tuladhar',
+    author_email='praj@sailthru.com',
+    url='https://github.com/sailthru/sailthru-python-client',
+    license='MIT License',
+    include_package_data=True,
+    zip_safe=True
+)
