cf0da2fe0b44a00b7cc31d72794777f20256fb1f
Prajwal Tuladhar <praj@infynyxx.com>
2012-06-17 14:29:22 -0400

    windows doesn't like ./

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(name='sailthru-client',
     version='2.0.1',
     packages=find_packages(),
     description='Python client for Sailthru API',
-    long_description=open('./README.md').read(),
+    long_description=open('README.md').read(),
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Topic :: Utilities",
