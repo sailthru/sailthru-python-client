5fd4d3d70eeba5ef05512cb892e9a9e022eb7c8e
Dennis Yu <dennisyu319@gmail.com>
2013-06-14 22:20:16 -0400

    update version numbers

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sailthru-client', 
-    version='2.0.4',
+    version='2.1.1',
     packages=find_packages(),
     description='Python client for Sailthru API',
     long_description=open('README.md').read(),
