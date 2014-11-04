7c36149dfdc8a3ace769d3111389f460aa06628b
Isaul Vargas <ivargas@sailthru.com>
2014-11-03 21:52:24 -0500

    Bumped and tagged to version 2.1.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='sailthru-client', 
-    version='2.1.6',
+    version='2.1.7',
     packages=find_packages(),
     description='Python client for Sailthru API',
     long_description=open('README.md').read(),
