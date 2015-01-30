2229f02893f8e72749d1e53b3ca3c5f24cfb5e72
Isaul Vargas <ivargas@sailthru.com>
2015-01-30 11:47:01 -0500

    Updated metadata and version number

--- sailthru/__init__.py
+++ sailthru/__init__.py
@@ -3,7 +3,7 @@ from .sailthru_error import SailthruClientError
 from .sailthru_response import SailthruResponse, SailthruResponseError
 
 __author__ = 'Prajwal Tuladhar'
-__doc__ = 'Python binding for Sailthru API based on Request'
-__copyright__ = 'Copyright 2012, Sailthru Inc.'
+__doc__ = 'Python binding for Sailthru API based on Requests'
+__copyright__ = 'Copyright 2012-2015, Sailthru Inc.'
 __license__ = 'MIT'
-__version__ = '2.1.3'
+__version__ = '2.2.0'
