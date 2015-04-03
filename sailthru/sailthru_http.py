d6c2a85cff9ba102ed877a4445732d12a36cfee7
Josh Tilles <jtilles@sailthru.com>
2015-04-03 13:11:40 -0400

    Dedupe version string by importing `__version__`.
    I think it would be particularly painful if the `User-Agent` header
    string got out of sync with the rest of the package.
    
    To define the version in a *single* location, we'd have to adopt a
    strategy like the ones detailed in <https://packaging.python.org/en/latest/single_source_version.html>.

--- sailthru/sailthru_http.py
+++ sailthru/sailthru_http.py
@@ -2,6 +2,7 @@
 
 import platform
 import requests
+from sailthru import __version__
 from .sailthru_error import SailthruClientError
 from .sailthru_response import SailthruResponse
 
@@ -37,7 +38,7 @@ def sailthru_http_request(url, data, method, file_data = None):
     params = data if method != 'POST' else None
     body = data if method == 'POST' else None
     try:
-        headers = { 'User-Agent': 'Sailthru API Python Client %s; Python Version: %s' % ('2.2.0', platform.python_version())}
+        headers = { 'User-Agent': 'Sailthru API Python Client %s; Python Version: %s' % (__version__, platform.python_version())}
         response = requests.request(method, url, params = params, data = data, files = file_data, headers = headers, timeout = 10)
         if response.status_code is None:
             raise SailthruClientError(response.error)
